0d65c6174f262b833a770c22738179c9047847dc
Andreas Landerer <andreas.landerer@gmail.com>
2018-03-19 20:51:14 +0100

    Lecture 14: Added SearchBar to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyB56qzFLvC81F26YSqoLI9g1NNHup_VinU';
+
 const App = () => {
     // write your first JSX code
-    return <div>Hi</div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 }
 ReactDOM.render(<App />, document.querySelector('.container'));
