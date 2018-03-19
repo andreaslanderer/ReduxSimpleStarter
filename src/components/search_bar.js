9b0b480fa87bce28601948a160181588c042af44
Andreas Landerer <andreas.landerer@gmail.com>
2018-03-19 23:07:43 +0100

    Lecture 16: Registered on change event handler in order to react to users keying some text into out input component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     render() {
-        return <input/>;
+        // return <input onChange={this.onInputChanged} />;
+        return <input onChange={(event) => console.log(event.target.value)} />;
     }
+
+    // onInputChanged(event) {
+    //     console.log(event.target.value);
+    // }
 }
 
 export default SearchBar;
